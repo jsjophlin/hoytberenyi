60c5e9ec6fabf59fb7335908f17b32d6244262f9
Josiah Jophlin <jsjophlin@gmail.com>
2019-05-26 11:17:04 -0400

    Enlarge icon and add new links.

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,42 +5,37 @@ import siteLogo from "../../assets/img/site_logo.png";
 
 const NavBar = ({ toggleNavbar, isActive }) => (
   <nav className="navbar is-fixed-top" aria-label="main navigation">
-    <div className="inner is-flex">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <img src={siteLogo} />
-        </Link>
-        <button
-          className={`button navbar-burger ${isActive ? "is-active" : ""}`}
-          data-target="navMenu"
-          onClick={toggleNavbar}
-        >
-          <span />
-          <span />
-          <span />
-        </button>
-      </div>
-      <div
-        className={`navbar-menu ${isActive ? "is-active" : ""}`}
-        id="navMenu"
+    <div className="navbar-brand">
+      <Link to="/" className="navbar-item">
+        <img src={siteLogo} />
+      </Link>
+      <button
+        className={`button navbar-burger ${isActive ? "is-active" : ""}`}
+        data-target="navMenu"
+        onClick={toggleNavbar}
       >
-        <div className="navbar-end">
-          <Link className="navbar-item" to="/pricing">
-            Pricing
-          </Link>
-          <Link className="navbar-item" to="/portfolio">
-            Projects
-          </Link>
-          <div className="navbar-item">
-            <div className="field is-grouped">
-              <p className="control">
-                <Link className="button is-primary is-outlined" to="/contact">
-                  Contact Us
-                </Link>
-              </p>
-            </div>
-          </div>
-        </div>
+        <span />
+        <span />
+        <span />
+      </button>
+    </div>
+    <div className={`navbar-menu ${isActive ? "is-active" : ""}`} id="navMenu">
+      <div className="navbar-end">
+        <Link className="navbar-item" to="/">
+          Home
+        </Link>
+        <Link className="navbar-item" to="/">
+          About Us
+        </Link>
+        <Link className="navbar-item" to="/">
+          Services
+        </Link>
+        <Link className="navbar-item" to="/portfolio">
+          Projects
+        </Link>
+        <Link className="navbar-item" to="/contact">
+          Contact Us
+        </Link>
       </div>
     </div>
   </nav>
