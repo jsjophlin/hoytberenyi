b57b06dbd3d20cf6859e00f06d51a2a4ed386271
Josiah Jophlin <jsjophlin@gmail.com>
2019-06-01 14:52:13 -0400

    Styled footer and added shadows to avatars.

--- config.js
+++ config.js
@@ -3,6 +3,10 @@ module.exports = {
   siteTitleAlt: "HoytBerenyi", // Alternative site title for SEO.
   siteLogo: "/icons/icon-512x512.png", // Logo used for SEO and manifest.
   siteUrl: "https://hoytberenyi.netlify.com/", // Domain of your website without pathPrefix.
+  siteAddress: "P.O. Box 1470, Ladson, SC 29456",
+  siteEmail: "info@hoytberenyi.com",
+  sitePhone: "(843) 408.3546",
+  siteFax: "(843) 408.3546",
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
   siteDescription: "Engineers & Consultants", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml",
@@ -12,7 +16,8 @@ module.exports = {
   userTwitter: "jsjophlin",
   userLocation: "Charelston, SC",
   userDescription: "",
-  copyright: "Copyright © Hoyt + Berenyi 2018-2019. All Rights Reserved.", // Copyright string for the footer of the website and RSS feed.
+  copyright:
+    "Tel (843) 408.3546 | Fax (843) 408.3456 | P.O. Box 1470, Ladson, SC 29456 | Email: info@hoytberenyi.com", // Copyright string for the RSS feed.
   themeColor: "#00d1b2", // Used for setting manifest and progress theme colors.
   backgroundColor: "#ffffff" // Used for setting manifest background color.
 };
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,25 @@
 import React from "react";
 import config from "../../../config";
+import "../../assets/sass/footer.sass";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
         <div className="content has-text-centered">
-          <p>{config.copyright}</p>
           <p>
-            Powered by <a href="https://www.gatsbyjs.org">Gatsby</a> and{" "}
-            <a href="https://www.netlifycms.org">Netlify CMS</a> |{" "}
-            <a href="https://github.com/v4iv/gatsby-starter-business">
-              Github Repository
-            </a>
+            <span className="slice">
+              Tel: <a href="tel:1-843-408-3546">{config.sitePhone}</a>
+            </span>
+            <span className="divider">|</span>
+            <span className="slice">{`Fax: ${config.siteFax}`}</span>
+            <span className="divider">|</span>
+            <span className="slice">{config.siteAddress}</span>
+            <span className="divider">|</span>
+            <span className="slice">
+              Email:{" "}
+              <a href={`mailto:${config.siteEmail}`}>{config.siteEmail}</a>
+            </span>
           </p>
         </div>
       </div>
