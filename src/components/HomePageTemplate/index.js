25a9c35275c86ea346673f0e4575bd3fac0196c5
Josiah Jophlin <jsjophlin@gmail.com>
2019-06-03 22:14:07 -0400

    Updated shadows, added react-icons, and link colors.

--- src/components/HomePageTemplate/index.js
+++ src/components/HomePageTemplate/index.js
@@ -3,6 +3,7 @@ import Helmet from "react-helmet";
 import PropTypes from "prop-types";
 import Img from "gatsby-image";
 import { Link } from "gatsby";
+import { FaTwitter, FaInstagram } from "react-icons/fa";
 import ParallaxHero from "../ParallaxHero";
 import ProjectTiles from "../ProjectTiles";
 import ContactForm from "../ContactForm";
@@ -189,7 +190,22 @@ const HomePageTemplate = ({
                       please contact us at{" "}
                       <a href="tel:1-843-408-3546">843.408.3546</a>.
                     </p>
-                    <p>Social icons here...</p>
+                    <p>
+                      <a
+                        className="icon"
+                        title="twitter"
+                        href="https://twitter.com"
+                      >
+                        <FaTwitter />
+                      </a>
+                      <a
+                        className="icon"
+                        title="instagram"
+                        href="https://www.instagram.com"
+                      >
+                        <FaInstagram />
+                      </a>
+                    </p>
                     <h3>Career Opportunities</h3>
                     <p>
                       Hoyt + Berenyi is a great place to work. Please feel free
